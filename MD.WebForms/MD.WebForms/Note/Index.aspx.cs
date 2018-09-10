9177905c053faed12e1e89a36dff93b862188577
Арсений Барсегян <Arseni.Barsegyan@gmail.com>
2018-09-10 14:50:51 +0300

    displaying correct notes data fix

--- MD.WebForms/MD.WebForms/Note/Index.aspx.cs
+++ MD.WebForms/MD.WebForms/Note/Index.aspx.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.Linq;
-using System.Dynamic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -23,30 +22,37 @@ public partial class Note_Index : Page
                 var context = new DataContext(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
 
                 var notes = context.GetTable<Note>().Where(x => x.UserId == userId);
-                var allIds = from note in notes select note.Id;
 
-                var photos = context.GetTable<Photo>()
-                    .Where(selectedPhoto => allIds.Any(x => selectedPhoto.Id == x));
+                var photos = context.GetTable<Photo>();
+                var userPhotos = from n in notes
+                    join p in photos on n.Id equals p.NoteId
+                    orderby p.Id descending
+                    select new {p.Image, p.Name, p.Id, p.NoteId };
 
-                var photosGroupedByNoteId = from g in (from n in notes
-                                                       join p in photos on n.Id equals p.NoteId
-                                                       orderby p.Id descending
-                                                       select new
-                                                       {
-                                                           n.Id,
-                                                           p.Image
-                                                       })
-                                            group g by g.Id;
 
-                var viewResult = new List<object>();
 
-                foreach (var group in photosGroupedByNoteId)
+                var result = from t in (from n in notes
+                        join p in userPhotos on n.Id equals p.NoteId
+                        orderby p.Id descending
+                        select new
+                        {
+                            n.Id,
+                            n.Date,
+                            n.Description,
+                            p.Image
+                        })
+                    group t by t.Id;
+
+                var res = new List<object>();
+
+                foreach (var group in result)
                 {
+                    Console.WriteLine(group.Key);
                     var firstItemInAGroup = group.FirstOrDefault();
-                    viewResult.Add(new { group.Key, firstItemInAGroup.Image });
+                    res.Add(new { group.Key, firstItemInAGroup.Image, firstItemInAGroup.Description, firstItemInAGroup.Date });
                 }
 
-                ListView.DataSource = viewResult.AsQueryable();
+                ListView.DataSource = res.AsQueryable();
                 ListView.DataBind();
             }   
         }
